b7869ad02c6e5dee50fcea07ab80301650b6a8e1
Alessio <belcastroalessio@outlook.it>
2023-09-20 17:37:35 +0200

    main

--- java.js
+++ java.js
@@ -1,3 +1,16 @@
+function titoli(){
+  let titoli = document.querySelectorAll(".title");
+  let body = document.querySelector(".modal-body");
+
+  if(body.innerHTML == ""){
+    for (let i = 0; i < titoli.length; i++) {
+      body.innerHTML += `<p>${titoli[i].innerHTML}</p>`
+    }
+  }
+
+  
+}
+
 const fetchMusic = (query, id) => {
   const section = document.querySelector(`#${id}`)
 
@@ -14,14 +27,17 @@ const fetchMusic = (query, id) => {
       section.classList.remove("d-none")
       for (let i = 0; i < music.slice(0,4).length; i++) {
         const element = music[i]
-        row.innerHTML += `<div class='col col-3'> <img class='w-100 rounded' src='${element.album.cover_xl}'/> <p class="mt-2">${element.title}</p></div>`
+        row.innerHTML += `<div class='col col-3'> <img class='w-100 rounded' src='${element.album.cover_xl}'/> <p class="mt-2 title">${element.title}</p></div>`
       }
     })
     .catch((err) => console.log(err))
+
+    titoli();
 }
+
 window.onload = () => {
+  fetchMusic("Billie-eilish","Billie-eilish")
   fetchMusic("eminem", "eminem")
   fetchMusic("queen", "queen")
   fetchMusic("metallica", "metallica")
-  fetchMusic("Billie-eilish","Billie-eilish")
 }
